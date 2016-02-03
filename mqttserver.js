31c27560017292be7b7dd2cb3b775c75abbba703
Kiana Tennyson <ktenny@thoughtworks.com>
2016-02-03 12:56:23 -0500

    clientId for mongoClient, subscribe to motion

--- mqttserver.js
+++ mqttserver.js
@@ -1,33 +1,25 @@
 var mqtt    = require('mqtt');
 var mongoClient = require('mongodb').MongoClient;
+var uuid = require('uuid');
 
-//TODO verify whether PINGREQ packets are sent
-var mqttClient  = mqtt.connect('ws://atliot.com:8080');
+var mqttClient  = mqtt.connect('ws://atliot.com:8080', {clientId: 'mongoClient_' + uuid.v1()});
 
 var insertDocument = function(db, document, callback) {
   var collection = db.collection('rawdata');
   collection.insert(document, function(err, result) {
-    console.log("error is: " + err);
     callback(result);
   })
 }
  
 mqttClient.on('connect', function () {
-  console.log('connected to mqtt message broker');
-  mqttClient.subscribe('motion');
+  mqttClient.subscribe('topic');
 });
  
 mqttClient.on('message', function (topic, message) {
-  console.log('message received: ' + message);
+  console.log('message received: ' + message + ' on topic: ' + topic);
   var jsonDoc = JSON.parse(message);
   mongoClient.connect('mongodb://localhost:27017/motionsensor', function(err, db) {
-    console.log('connected to mongodb');
     insertDocument(db, jsonDoc, function(result){
-      if (result === null) {
-        console.log('successfully inserted document.');
-      }
-
-      //TODO don't believe this is how to properly close the connection.
       db.close();
     });
   });
