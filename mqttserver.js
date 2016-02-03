437b8c8a79ee3d9f8b457813842017c59c5f4caf
Kiana Tennyson <ktenny@thoughtworks.com>
2016-02-03 13:00:24 -0500

    less logging, actual topic name change

--- mqttserver.js
+++ mqttserver.js
@@ -12,15 +12,19 @@ var insertDocument = function(db, document, callback) {
 }
  
 mqttClient.on('connect', function () {
-  mqttClient.subscribe('topic');
+  console.log('mongoClient connected to mosquitto broker.')
+  mqttClient.subscribe('motion');
 });
  
 mqttClient.on('message', function (topic, message) {
-  console.log('message received: ' + message + ' on topic: ' + topic);
   var jsonDoc = JSON.parse(message);
   mongoClient.connect('mongodb://localhost:27017/motionsensor', function(err, db) {
     insertDocument(db, jsonDoc, function(result){
       db.close();
     });
   });
+});
+
+mqttClient.on('close', function() {
+  console.log('mongoClient disconnected from mosquitto broker')
 });
